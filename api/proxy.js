0d93bd889e51a364aa43f33809751c8c7be148f9
Arlando Sugiyanto <arlando@example.com>
2025-06-20 11:37:34 +0700

    fix: add JSON content-type in proxy

--- api/proxy.js
+++ api/proxy.js
@@ -6,6 +6,8 @@ export default async function handler(req, res) {
   try {
     const response = await fetch(url);
     const data = await response.text();
+
+    res.setHeader('Content-Type', 'application/json');
     res.status(200).send(data);
   } catch (error) {
     res.status(500).json({ result: 'error', message: error.message });
